1c4c33c20c45b3fa564cd4737d24800ab10e7eef
Jason Hoong Kit Lee <hklee@pivotal.io>
2019-03-07 16:57:27 +0800

    Initial Commit

---
+++ CICD-pivotal-bank-demo/bgd-deploy.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+echo "Environment Variables"
+env
+echo "End env variables"
+
+echo "Setting up CF Cli"
+# ...or Linux 64-bit binary
+curl -L "https://packages.cloudfoundry.org/stable?release=linux64-binary&source=github" | tar -zx
+# ...move it to /usr/local/bin or a location you know is in your $PATH
+mv cf /usr/local/bin
+# ...copy tab completion file on Ubuntu (takes affect after re-opening your shell)
+curl -o /usr/share/bash-completion/completions/cf https://raw.githubusercontent.com/cloudfoundry/cli/master/ci/installers/completion/cf
+# ...and to confirm your cf CLI version
+cf --version
+cf install-plugin -f -r CF-Community "blue-green-deploy"
+#Log in to cloud foundry
+echo "Running: cf login -a ${CF_API} --skip-ssl-validation -o ${CF_ORG} -s ${CF_SPACE}"
+cf login -a ${CF_API} --skip-ssl-validation -o ${CF_ORG} -s ${CF_SPACE} -u ${CF_USERNAME} -p ${CF_PASSWORD}
+
+#Download artifact
+echo "Downloading artifact to deploy"
+cd version-resource-gist
+
+#Download manifest and associated files
+echo "Contents in version-resource-gist: $(ls)"
+echo "Downloading from Bintray"
+Version=$(cat *release-version)
+Group=$(mvn help:evaluate -Dexpression=project.groupId -q -DforceStdout -f=pom.xml | tr . /)
+Artifact=$(mvn help:evaluate -Dexpression=project.artifactId -q -DforceStdout -f=pom.xml)
+
+FILE_PATH="${Group}/${Artifact}/${Version}/${Artifact}-${Version}.jar"
+echo "Downloading artifact from: https://dl.bintray.com/xlpeabrain/maven-repo/${FILE_PATH}"
+curl -L "https://dl.bintray.com/xlpeabrain/maven-repo/${FILE_PATH}" -o ${Artifact}.jar
+
+cd ..
+cp version-resource-gist/* built-core-op/
+mkdir built-core-op/target
+mv built-core-op/${Artifact}.jar built-core-op/target/
+
+cat built-core-op/manifest.yml
+
+echo "Contents in built-core-op: $(ls built-core-op)"
+
+cd built-core-op
+
+cf bgd ${CF_APP_NAME} -delete-old-apps
---
+++ CICD-pivotal-bank-demo/build-release.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+M2_HOME="${HOME}/.m2"
+M2_CACHE="${PWD}/maven"
+BASE=${PWD}
+
+echo "Generating symbolic links for caches"
+[[ -d "${M2_CACHE}" && ! -d "${M2_HOME}" ]] && ln -s "${M2_CACHE}" "${M2_HOME}"
+
+cd git-resource-core
+cd $WORKING_DIR
+mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion} versions:commit
+mvn clean package
+cp target/core.jar ${BASE}/built-core/
+cp pom.xml ${BASE}/built-core/
+ls ${BASE}/built-core
+
+STATUS=$?
+echo $STATUS
+if [ "$STATUS" != 0 ]; then
+    mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}-SNAPSHOT versions:commit
+else
+    echo "Setting up Git"
+    apt-get -y update
+    apt-get -y upgrade
+    apt-get -y install git
+
+    git --version
+    git config --global user.email "concourse@nomail.com"
+    git config --global user.name "Concourse"
+    cd ${BASE}
+    git clone version-resource-gist updated-gist
+
+    cd updated-gist
+#    echo $(ls)
+    echo $(mvn help:evaluate -Dexpression=project.version -q -DforceStdout -f=../git-resource-core/pom.xml) > ${CF_APP_NAME}-release-version
+    cp ${BASE}/git-resource-core/${WORKING_DIR}/manifest.yml .
+    cp ${BASE}/git-resource-core/${WORKING_DIR}/pom.xml .
+    rm -rf core.jar
+
+    echo "Contents for gist:$(ls)"
+
+    git add .
+    git commit -m "Bumped version"
+fi
